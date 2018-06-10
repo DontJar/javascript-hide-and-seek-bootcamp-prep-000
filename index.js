735f5a88422f5d45500d5bd9d03a186db96ee789
Jon Schulman <schulman99@gmail.com>
2018-06-10 23:52:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,13 +10,9 @@ function nestedTarget() {
 
 function increaseRankBy(n){
   const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
-  for (let i = 1; i < lis.length; i++) {
-  lis.innerHTML = (lis + n).toString()
+  for (let i = 0; i < lis.length; i++) {
+    lis.innerHTML = (lis + n).toString()
 }
-  
-  
-//increases the ranks in all of the .ranked-list 's' by 'n'
-
 }
 
 function deepestChird() {
