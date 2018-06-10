3c5517e6790ae053f839ad8495106cafb4626761
Jon Schulman <schulman99@gmail.com>
2018-06-10 23:07:49 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,19 @@
+function getFirstSelector(selector) {
+//accepts a selector and returns first elements that match
+
+}
+
+function nestedTarger() {
+//pulls a .target out of #nested
+
+}
+
+function increaseRankBy(n){
+//increases the ranks in all of the .ranked-list 's' by 'n'
+
+}
+
+function deepestChird() {
+//pulls out the most deeply nested child from 'div#grand-node'
+  
+}
