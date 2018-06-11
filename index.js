f5d5322acdfa7248ae73a959d23ae848f260bb73
Jon Schulman <schulman99@gmail.com>
2018-06-11 00:01:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ function nestedTarget() {
 }
 
 function increaseRankBy(n){
-  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
-  for (let i = 0; i < lis.length; i++) {
-    lis[i].innerHTML = (parseInt(lis[i]) + n).toString
+  let newRank = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+  for (let i = 0; i < newRank.length; i++) {
+    newRank[i].innerHTML = (parseInt(newRank[i]) + n).toString
 }
 }
 
