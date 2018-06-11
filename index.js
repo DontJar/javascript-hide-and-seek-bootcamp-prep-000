5a53c6ffb686093241b018c1845d051793a6d2d8
Jon Schulman <schulman99@gmail.com>
2018-06-11 01:15:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,20 +15,6 @@ function increaseRankBy(n){
 }
 }
 
-
-
-/*
-function increaseRankBy(n) {
-  const rankedLists = document.querySelectorAll('.ranked-list')
-
-  for (let i = 0, l = rankedLists.length; i < l; i++) {
-    let children = rankedLists[i].children
-
-    for (let j = 0, k = children.length; j < k; j++) {
-      children[j].innerHTML = parseInt(children[j].innerHTML) + n
-*/
-
-
 function deepestChird() {
 //pulls out the most deeply nested child from 'div#grand-node'
   
