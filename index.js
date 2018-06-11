fbfb5c15ded1a2aa79c2cebbe07df0c3a48dc04d
Jon Schulman <schulman99@gmail.com>
2018-06-11 01:51:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function deepestChild() {
 // I dont like this solution.  Feels like cheating.
 
 
-let current = document.querySelectorAll('#grand-node')
+let current = document.getElementsByID('#grand-node')
 let next = current.children[0]
 while (next) {
     current = next
