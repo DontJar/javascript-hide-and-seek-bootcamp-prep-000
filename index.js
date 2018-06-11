e4379f39ddf08e3fe278f88d22bfed5641257f8a
Jon Schulman <schulman99@gmail.com>
2018-06-11 01:35:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function increaseRankBy(n){
 }
 
 function deepestChild() {
-  document.querySelector('#grand-node div div div div')}
+  return document.querySelector('#grand-node div div div div')}
 
 
 //  let current = document.querySelectorAll('#grand-node')
